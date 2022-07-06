6ee9885885015a838ada3871d0a94f006d83292f
Egor Mukhametvafin <egor190420000@gmail.com>
2022-07-06 18:25:07 +0300

    Add files via upload

---
+++ tgbot/config.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    host: str
+    password: str
+    user: str
+    database: str
+
+
+@dataclass
+class TgBot:
+    token: str
+    admin_ids: str
+    use_redis: bool
+
+
+@dataclass
+class Miscellaneous:
+    app_base: str
+    web_host: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DbConfig
+    misc: Miscellaneous
+
+
+def load_config(path: str = None):
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+        tg_bot=TgBot(
+            token=env.str("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMINS"))),
+            use_redis=env.bool("USE_REDIS"),
+        ),
+        db=DbConfig(
+            host=env.str('DB_HOST'),
+            password=env.str('DB_PASS'),
+            user=env.str('DB_USER'),
+            database=env.str('DB_NAME')
+        ),
+        misc=Miscellaneous(
+            app_base=env.str('APP_BASE_URL'),
+            web_host=env.str('WEB_HOST')
+        )
+    )
